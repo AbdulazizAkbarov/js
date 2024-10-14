71d715f92ddce4c2b82415d467483901734bdea0
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-14 18:57:14 +0500

    Finished

---
+++ main.js
@@ -0,0 +1,100 @@
+//Vazifa 1  Personal Information ! //
+
+
+
+// let isim =prompt("Ismingizni Kirgizing");
+// let familya =prompt("Familyangizni  Kirgizing");
+// let Otasining_ism =prompt("Otangizni Ismini Kirgizing");
+// let Onasining_ism =prompt("Onangzni Ismini Kirgizing");
+// let tugilgan_kun =prompt("Tugilgan kuningizni Kirgizing");
+// let telefon_raqam =prompt(" Telefon Raqamingizni Kirgizing");
+// let yashash_manzili =prompt("Yashash Manzilingizni Kzirgizing");
+// let kasbi =prompt("Kasbingizni  Kirgizing");
+// let qiziqishi =prompt("Qiziqshingizni Kzirgizing");
+// let qobilyati =prompt("Qobilyatingizni Kirgizing");
+
+// console.log( `
+
+
+// Familyangiz: ${familya},
+// Ismingiz: ${isim},
+// Otangizning Ismi: ${Otasining_ism},
+// Onangizning Ismi: ${Onasining_ism},
+// Tugilgan Kuningiz: ${tugilgan_kun},
+// Yashash Manzilingiz: ${yashash_manzili},
+// Kasbingiz: ${kasbi},
+// Qiziqishingiz: ${qiziqishi},
+// Qobilyatingiz: ${qobilyati},
+//  `
+// );
+
+// alert(`
+// Familyangiz: ${familya},
+// Ismingiz: ${isim},
+// Otangizning Ismi: ${Otasining_ism},
+// Onangizning Ismi: ${Onasining_ism},
+// Tugilgan Kuningiz: ${tugilgan_kun},
+// Yashash Manzilingiz: ${yashash_manzili},
+// Kasbingiz: ${kasbi},
+// Qiziqishingiz: ${qiziqishi},
+// Qobilyatingiz: ${qobilyati},
+
+// `);
+
+
+
+
+//1 Vazifani Tugashi//
+
+
+
+
+
+//2 Vazifani Boshlanishi Shoppping Calculator//
+
+    // let olma =+prompt ("Olam Narhini Kirgizing");
+    // let anor =+prompt ("Anor Narhini Kirgizing");
+    // let behi =+prompt ("Behi Narhini Kirgizing");
+    // let nok =+prompt ("Nok Narhini Kirgizing");
+    // let uzum =+prompt ("Uzum Narhini Kirgizing");
+    // let qulpnay =+prompt ("Qulpnay Narhini Kirgizing");
+    // let malina =+prompt ("Malina Narhini Kirgizing");
+    // let gilos =+prompt ("Gilos Narhini Kirgizing");
+    // let banan =+prompt ("Banan Narhini Kirgizing");
+    // let ananas =+prompt ("Ananas Narhini Kirgizing");
+
+    // console.log("Umumiy Summa:", (anor + olma + behi + nok+uzum + qulpnay + malina+gilos + banan + ananas));
+
+
+
+    //2 Vazifani tugashi //
+
+
+
+//3-Vazifani Boshlanishi Avarage Grade Calculator //
+
+    // let matematika =+prompt ("Mtematikadan Balinigizni kirgizing");
+    // let onatili =+prompt ("Ona tilidan Balinigizni Kirgizing");
+    // let adabiyot =+prompt ("Adabiyot Balingizni Kirgizing");
+    // let fizika =+prompt ("Fizikadan Balingizni Kirgizing");
+    // let tarix =+prompt ("Tarixdan Balingizni Kirgizing");
+    // let rustili =+prompt ("Rustilidan Balingizng Kirgizing");
+    // let nemistili =+prompt ("Nemis Tilidan Balingizni Kirgizing");
+    // let informatika =+prompt ("Informatikadan Balingizni Kirgizing");
+    // let jismoniytarbiya =+prompt ("Jismoniytarbiyadan Balingizni Kirgizing");
+    
+
+    // console.log("Umumiy Balninig Orta Arifmetigi:", (onatili + matematika + adabiyot + fizika+tarix + rustili + nemistili+informatika + jismoniytarbiya )/10);
+
+
+
+    //3 Vazifani tugashi //
+
+
+
+
+    
+
+
+
+
